23ee095c951e802cf6adc6509cae35ae08f35dff
Max Roth <maxroth@Maxs-MacBook-Air.local>
2023-07-22 18:07:35 +0200

    two bot version done

--- Chess-Challenge/src/Evil Bot/EvilBot.cs
+++ Chess-Challenge/src/Evil Bot/EvilBot.cs
@@ -1,54 +1,140 @@
 ﻿using ChessChallenge.API;
 using System;
+using System.Linq;
 
 namespace ChessChallenge.Example
 {
-    // A simple bot that can spot mate in one, and always captures the most valuable piece it can.
-    // Plays randomly otherwise.
     public class EvilBot : IChessBot
     {
         // Piece values: null, pawn, knight, bishop, rook, queen, king
-        int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
+        private readonly int[] pieceValues = { 0, 1, 3, 3, 5, 9, 10 };
 
         public Move Think(Board board, Timer timer)
         {
-            Move[] allMoves = board.GetLegalMoves();
-
-            // Pick a random move to play if nothing better is found
-            Random rng = new();
-            Move moveToPlay = allMoves[rng.Next(allMoves.Length)];
-            int highestValueCapture = 0;
+            var allMoves = board.GetLegalMoves().OrderByDescending(m => MoveValue(board, m)).ThenBy(m => Guid.NewGuid()).ToArray();  // Order by move value, then shuffle equally valued moves
+            Move bestMove = allMoves[0];
+            int bestValue = int.MinValue;
 
             foreach (Move move in allMoves)
             {
-                // Always play checkmate in one
-                if (MoveIsCheckmate(board, move))
-                {
-                    moveToPlay = move;
-                    break;
-                }
+                board.MakeMove(move);
+                int moveValue = Minimax(board, 4, bestValue, int.MaxValue, true);
+                board.UndoMove(move);
 
-                // Find highest value capture
-                Piece capturedPiece = board.GetPiece(move.TargetSquare);
-                int capturedPieceValue = pieceValues[(int)capturedPiece.PieceType];
-
-                if (capturedPieceValue > highestValueCapture)
+                if (moveValue > bestValue)
                 {
-                    moveToPlay = move;
-                    highestValueCapture = capturedPieceValue;
+                    bestValue = moveValue;
+                    bestMove = move;
                 }
             }
 
-            return moveToPlay;
+            var movingPiece = board.GetPiece(bestMove.StartSquare);
+            int movingPieceValue = pieceValues[(int)movingPiece.PieceType];
+            Console.WriteLine($"Chosen move: {bestMove}, Moving piece value: {movingPieceValue}");
+
+            return bestMove;
         }
 
-        // Test if this move gives checkmate
+        int MoveValue(Board board, Move move)
+        {
+            if (MoveIsCheckmate(board, move))
+                return int.MaxValue;
+            if (BeneficialCapture(board, move))
+                return pieceValues[(int)board.GetPiece(move.TargetSquare).PieceType] - pieceValues[(int)board.GetPiece(move.StartSquare).PieceType];
+            if (SafeMove(board, move))
+                return 0;
+            return int.MinValue;
+        }
+
         bool MoveIsCheckmate(Board board, Move move)
         {
             board.MakeMove(move);
-            bool isMate = board.IsInCheckmate();
+            var isMate = board.IsInCheckmate();
             board.UndoMove(move);
             return isMate;
+        }
+
+        bool BeneficialCapture(Board board, Move move)
+        {
+            var ourPiece = board.GetPiece(move.StartSquare);
+            var ourPieceValue = pieceValues[(int)ourPiece.PieceType];
+
+            if (board.GetPiece(move.TargetSquare) == null)
+                return false;
+
+            var theirPiece = board.GetPiece(move.TargetSquare);
+            var theirPieceValue = pieceValues[(int)theirPiece.PieceType];
+
+            return ourPieceValue <= theirPieceValue;
+        }
+
+        bool SafeMove(Board board, Move move)
+        {
+            board.MakeMove(move);
+            var isSafe = !board.GetLegalMoves().Any(m => m.TargetSquare == move.TargetSquare);
+            board.UndoMove(move);
+
+            return isSafe;
+        }
+
+        int Minimax(Board board, int depth, int alpha, int beta, bool maximizingPlayer)
+        {
+            if (depth == 0)
+            {
+                return EvaluateBoard(board);
+            }
+
+            var moves = board.GetLegalMoves().OrderByDescending(m => MoveValue(board, m)).ThenBy(m => Guid.NewGuid()).ToArray();  // Order by move value, then shuffle equally valued moves
+
+            if (maximizingPlayer)
+            {
+                int maxEval = int.MinValue;
+
+                foreach (var move in moves)
+                {
+                    board.MakeMove(move);
+                    int eval = Minimax(board, depth - 1, alpha, beta, false);
+                    board.UndoMove(move);
+                    maxEval = Math.Max(maxEval, eval);
+
+                    alpha = Math.Max(alpha, eval);
+                    if (beta <= alpha)
+                        break;
+                }
+                return maxEval;
+            }
+            else
+            {
+                int minEval = int.MaxValue;
+
+                foreach (var move in moves)
+                {
+                    board.MakeMove(move);
+                    int eval = Minimax(board, depth - 1, alpha, beta, true);
+                    board.UndoMove(move);
+                    minEval = Math.Min(minEval, eval);
+
+                    beta = Math.Min(beta, eval);
+                    if (beta <= alpha)
+                        break;
+                }
+                return minEval;
+            }
+        }
+
+        int EvaluateBoard(Board board)
+        {
+            int score = 0;
+            foreach (var pieceList in board.GetAllPieceLists())
+            {
+                foreach (var piece in pieceList)
+                {
+                    // Adjust the score based on the piece color
+                    int pieceScore = pieceValues[(int)piece.PieceType];
+                    score += piece.IsWhite ? pieceScore : -pieceScore;
+                }
+            }
+            return score;
         }
     }
 }
--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,138 @@
 ﻿using ChessChallenge.API;
+using System;
 
 public class MyBot : IChessBot
 {
+    // Piece values: null, pawn, knight, bishop, rook, queen, king
+    private int[] pieceValues = { 0, 1, 3, 3, 5, 9, 10 };
+
     public Move Think(Board board, Timer timer)
     {
-        Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        Move[] allMoves = board.GetLegalMoves();
+
+        // Initialize move to play
+        Move moveToPlay = new Move();
+        int highestValueCapture = 0;
+
+        foreach (Move move in allMoves)
+        {
+           
+            
+            // Always play checkmate in one
+            if (MoveIsCheckmate(board, move))
+            {
+                moveToPlay = move;
+                break;
+            }
+             if (move.IsPromotion && (int)move.PromotionPieceType == 5)
+            {
+                return move;
+            }
+
+        
+            Piece capturedPiece = board.GetPiece(move.TargetSquare);
+            int capturedPieceValue = pieceValues[(int)capturedPiece.PieceType];
+
+            if (capturedPieceValue > highestValueCapture && BeneficialCapture(board, move))
+            {
+                moveToPlay = move;
+                highestValueCapture = capturedPieceValue;
+            }
+        }
+
+
+        // If no defensive moves found, find a safe move
+        if (moveToPlay.IsNull)
+        {
+            Random rng = new();
+            for (int i = 0; i < allMoves.Length; i++)
+            {
+                moveToPlay = allMoves[rng.Next(allMoves.Length)];
+                if (SafeMove(board, moveToPlay))
+                {
+                    break;
+                }
+            }
+        }
+
+        Piece movingPiece = board.GetPiece(moveToPlay.StartSquare);
+        int movingPieceValue = pieceValues[(int)movingPiece.PieceType];
+        Console.WriteLine($"Chosen move: {moveToPlay}, Moving piece value: {movingPieceValue}");
+
+        return moveToPlay;
     }
+
+    // Test if this move gives checkmate
+    bool MoveIsCheckmate(Board board, Move move)
+    {
+        board.MakeMove(move);
+        bool isMate = board.IsInCheckmate();
+        board.UndoMove(move);
+        return isMate;
+    }
+
+    // Check if a capture is beneficial
+    bool BeneficialCapture(Board board, Move move)
+    {
+        // Check the value of our piece
+        Piece ourPiece = board.GetPiece(move.StartSquare);
+        int ourPieceValue = pieceValues[(int)ourPiece.PieceType];
+
+        // Execute the move
+        board.MakeMove(move);
+
+        // Get all of the opponent's legal moves
+        Move[] opponentMoves = board.GetLegalMoves();
+
+        bool isBeneficial = true;
+
+        foreach (Move opponentMove in opponentMoves)
+        {
+            // If the opponent can capture our piece on their next move, check the value of the capture
+            if (opponentMove.TargetSquare == move.TargetSquare)
+            {
+                Piece capturingPiece = board.GetPiece(opponentMove.StartSquare);  // Change this to StartSquare to get the piece that will capture us.
+                int capturingPieceValue = pieceValues[(int)capturingPiece.PieceType];
+
+                // If the value of the capturing piece is less than the value of our piece, then the capture is not beneficial
+                if (capturingPieceValue < ourPieceValue)
+                {
+                    isBeneficial = false;
+                }
+            }
+        }
+
+        // Revert the move before returning
+        board.UndoMove(move);
+        return isBeneficial;
+    }
+
+    // Check if a move is safe
+    bool SafeMove(Board board, Move move)
+    {
+        // Execute the move
+        board.MakeMove(move);
+
+        // Get all of the opponent's legal moves
+        Move[] opponentMoves = board.GetLegalMoves();
+
+        // Assume the move is safe until proven otherwise
+        bool isSafe = true;
+
+        foreach (Move opponentMove in opponentMoves)
+        {
+            // If the opponent can capture our piece on their next move, the move is not safe
+            if (opponentMove.TargetSquare == move.TargetSquare)
+            {
+                isSafe = false;
+                break;
+            }
+        }
+
+        // Revert the move before returning
+        board.UndoMove(move);
+        return isSafe;
+    }
+
+  
 }
